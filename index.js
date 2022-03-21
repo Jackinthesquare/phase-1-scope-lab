e883d71978c515b75d2296328d63b3a9ab0eb5e9
Jacky Liang <asheriffy@gmail.com>
2022-03-21 09:42:37 -0400

    commit tree d9c6ba13a51884f2b87505c34ab722ae31440d39

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+// const upperCaseCustomerName = () => {
+//     return customerName = customerName.toUpperCase()
+// } DOES NOT WORK
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase()
+}
+
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 5
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 3
+}
